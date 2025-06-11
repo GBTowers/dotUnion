088cd80cb0e1799689237ee3dcf9d1882a745274
Jorge-Torres <jorgearmandoz.x@gmail.com>
2025-06-10 21:08:09 -0600

    Migrate main project

--- dotUnion.Integration/UnionIntegration.cs
+++ dotUnion.Integration/UnionIntegration.cs
@@ -1,23 +1,23 @@
-﻿using dotUnion.Attributes;
-
-namespace dotUnion.Integration;
-
-[Union]
-public partial record Result<T, TE>
-{
-	partial record Ok(T Value);
-	partial record Err(TE Error);
-}
-
-public class UnionIntegration
-{
-	[Fact]
-	public void MatchMethodWorksOnDerivedClasses()
-	{
-		Result<int, string> result = 4;
-
-		string s = result.Match(f1: success => success.Value.ToString(), f2: failure => failure.Error);
-
-		Assert.Equal(expected: "4", actual: s);
-	}
-}
+﻿// using dotUnion.Attributes;
+//
+// namespace dotUnion.Integration;
+//
+// [Union]
+// public partial record Result<T, TE>
+// {
+// 	partial record Ok(T Value);
+// 	partial record Err(TE Error);
+// }
+//
+// public class UnionIntegration
+// {
+// 	[Fact]
+// 	public void MatchMethodWorksOnDerivedClasses()
+// 	{
+// 		Result<int, string> result = 4;
+//
+// 		string s = result.Match(f1: success => success.Value.ToString(), f2: failure => failure.Error);
+//
+// 		Assert.Equal(expected: "4", actual: s);
+// 	}
+// }
